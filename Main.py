7d6af99a7bacca1075429dfa55394a904139e491
nardolillos <nardolillosantino@gmail.com>
2023-06-15 09:30:47 -0400

    cleaned up changes made

--- Main.py
+++ Main.py
@@ -35,24 +35,6 @@ class User: # TC: this is the intro
 class Student(User):
     def displayflow_student1(self):
         print("display the first students flow chart")
-    def displayflow_student2(self):
-        print("display the second students flow chart")
-    def displayflow_student3(self):
-        print("display the third students flow chart ")
-    def displayflow_student4(self):
-        print("display the fouth students flow chart ")
-    def displayflow_student5(self):
-        print("display the fifth students flow chart ")
-    def displayflow_student6(self):
-        print("display the sixth students flow chart ")
-    def displayflow_student7(self):
-        print("display the seventh students flow chart ")
-    def displayflow_student8(self):
-        print("display the 8th students flow chart ")
-    def displayflow_student9(self):
-        print("display the ninth students flow chart ")
-    def displayflow_student10(self):
-        print("display the tenth students flow chart ")
     def add_class(self):
         print("Here they can add a class")
     def delete_class(self):
@@ -62,47 +44,11 @@ class Student(User):
 class Instructor(User):
     def displayflow_student1(self):
         print("display the first students flow chart")
-    def displayflow_student2(self):
-        print("display the second students flow chart")
-    def displayflow_student3(self):
-        print("display the third students flow chart ")
-    def displayflow_student4(self):
-        print("display the fouth students flow chart ")
-    def displayflow_student5(self):
-        print("display the fifth students flow chart ")
-    def displayflow_student6(self):
-        print("display the sixth students flow chart ")
-    def displayflow_student7(self):
-        print("display the seventh students flow chart ")
-    def displayflow_student8(self):
-        print("display the 8th students flow chart ")
-    def displayflow_student9(self):
-        print("display the ninth students flow chart ")
-    def displayflow_student10(self):
-        print("display the tenth students flow chart ")
 
 # Creation of Child Class (Admin)
 class Admin(User):
     def displayflow_student1(self):
         print("display the first students flow chart")
-    def displayflow_student2(self):
-        print("display the second students flow chart")
-    def displayflow_student3(self):
-        print("display the third students flow chart ")
-    def displayflow_student4(self):
-        print("display the fouth students flow chart ")
-    def displayflow_student5(self):
-        print("display the fifth students flow chart ")
-    def displayflow_student6(self):
-        print("display the sixth students flow chart ")
-    def displayflow_student7(self):
-        print("display the seventh students flow chart ")
-    def displayflow_student8(self):
-        print("display the 8th students flow chart ")
-    def displayflow_student9(self):
-        print("display the ninth students flow chart ")
-    def displayflow_student10(self):
-        print("display the tenth students flow chart ")
     def add_class(self):
         print("Here they can add a class")
     def delete_class(self):
@@ -320,6 +266,7 @@ def insert_data():
         except Exception as e:
             print("Error: Failed to insert data.")
             print(e)
+
 def update_data():
 	print("Selected: Update Data")
 	databases = ["ADMIN", "INSTRUCTOR", "STUDENT", "COURSE"]
@@ -371,61 +318,7 @@ def delete_data():
     deleteText = "DELETE FROM " + databases[dbSelection] + " WHERE " + databasesKeyVal[dbSelection] + " = " + deleteSelection;
     cursor.execute(deleteText)
 
-# User Demonstration
-student1 = User("Santino", "Nardolillo", "114")
-student1.intro()
-print()
-
-# Admin Demonstration
-adminuser = Admin("Jim", "Joe", "654") #make dean
-#dont display just change  (dean, academic cordinator, etc. ) be able to change the pre-req the arrow will be deleted 
-adminuser.intro()
-adminuser.displayflow_student1()
-adminuser.displayflow_student2()
-adminuser.displayflow_student3()
-adminuser.displayflow_student4()
-adminuser.displayflow_student5()
-adminuser.displayflow_student6()
-adminuser.displayflow_student7()
-adminuser.displayflow_student8()
-adminuser.displayflow_student9()
-adminuser.displayflow_student10()
-adminuser.add_class()
-adminuser.delete_class()
-print() 
-
-# Student Demonstration
-studentuser = Student("John", "Doe", "123")
-studentuser.intro()
-studentuser.displayflow_student1()
-studentuser.displayflow_student2()
-studentuser.displayflow_student3()
-studentuser.displayflow_student4()
-studentuser.displayflow_student5()
-studentuser.displayflow_student6()
-studentuser.displayflow_student7()
-studentuser.displayflow_student8()
-studentuser.displayflow_student9()
-studentuser.displayflow_student10()
-studentuser.add_class()
-studentuser.delete_class()
-print()
-
-# Instructor Demonstration
-#just view 
-instructoruser = Instructor("Jeremy", "Smith", "456")
-instructoruser.displayflow_student1()
-instructoruser.displayflow_student2()
-instructoruser.displayflow_student3()
-instructoruser.displayflow_student4()
-instructoruser.displayflow_student5()
-instructoruser.displayflow_student6()
-instructoruser.displayflow_student7()
-instructoruser.displayflow_student8()
-instructoruser.displayflow_student9()
-instructoruser.displayflow_student10()
-print()
-
+# MAIN LOOP FOR FILE EXECUTION
 exit = False
 while (exit == False):
     print("0 - Create new table\n1 - Search by parameter\n2 - Insert new entry to table\n3 - Update existing table entry\n4 - Remove existing table entry\n5 - Print all tables\n6 - Exit")
@@ -468,3 +361,31 @@ while (exit == False):
 database.commit()
 
 database.close()
+
+# User Demonstration
+student1 = User("Santino", "Nardolillo", "114")
+student1.intro()
+print()
+
+# Admin Demonstration
+adminuser = Admin("Jim", "Joe", "654") #make dean
+#dont display just change  (dean, academic cordinator, etc. ) be able to change the pre-req the arrow will be deleted 
+adminuser.intro()
+adminuser.displayflow_student1()
+adminuser.add_class()
+adminuser.delete_class()
+print() 
+
+# Student Demonstration
+studentuser = Student("John", "Doe", "123")
+studentuser.intro()
+studentuser.displayflow_student1()
+studentuser.add_class()
+studentuser.delete_class()
+print()
+
+# Instructor Demonstration
+#just view 
+instructoruser = Instructor("Jeremy", "Smith", "456")
+instructoruser.displayflow_student1()
+print()
